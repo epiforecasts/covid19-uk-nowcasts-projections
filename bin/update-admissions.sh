105bc26cbfd46eba8c281697a645beef30857c84
Sam Abbott <s.e.abbott12@gmail.com>
2022-02-18 17:49:19 +0000

    add admissions progress messages

--- bin/update-admissions.sh
+++ bin/update-admissions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ##Run national/regional nowcast/forecast
+printf "Estimating Rt from admissions data\n"
 Rscript forecast/update-admissions.R
 
 ## Format the forecast for reporting
+printf "Processing admissions projections\n"
 Rscript format-forecast/update-admissions.R
 
