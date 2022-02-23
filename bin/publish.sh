dd9c4cb66e2a545e74bf788b91735ef263ed48f5
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-02-23 00:04:40 +0000

    publication script

---
+++ bin/publish.sh
@@ -0,0 +1,12 @@
+git add -A
+git commit -m"Update $(date)"
+git pull -Xours
+git push -v
+
+git branch -d gh-pages
+git checkout --orphan gh-pages
+git rm -rf .
+git add docs/nowcast-and-projections.html
+git commit -m "Update report"
+git push --force origin gh-pages
+git checkout main
