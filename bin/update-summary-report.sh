1cda24bb7bae21a3aecfa2b9651c01ff247da82a
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-02-23 20:14:43 +0000

    move report to index

--- bin/update-summary-report.sh
+++ bin/update-summary-report.sh
@@ -3,4 +3,6 @@
 Rscript -e "rmarkdown::render(here::here('reports', 'nowcast-and-projections', 'report.Rmd'))"
 
 mkdir docs 2> /dev/null
-mv reports/nowcast-and-projections/report.html docs/nowcast-and-projections.html
+mv reports/nowcast-and-projections/report.html docs/index.html
+
+echo Report moved to docs/index.html
