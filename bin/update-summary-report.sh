360a5b8a629a41bc84a7269c5a94886121ed9d25
Sam Abbott <contact@samabbott.co.uk>
2022-03-23 18:44:31 +0000

    add severity report publishing

--- bin/update-summary-report.sh
+++ bin/update-summary-report.sh
@@ -6,3 +6,7 @@ mkdir docs 2> /dev/null
 mv reports/nowcast-and-projections/report.html docs/index.html
 
 echo Report moved to docs/index.html
+
+mv reports/severity/report.html docs/severity.html
+
+echo Severity report moved to docs/index.html
