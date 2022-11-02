201e6cdc5c364c359543fd6729567f92ff90db2f
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2022-11-02 14:09:54 +0000

    Update Wed  2 Nov 14:09:54 GMT 2022

--- bin/update-data.sh
+++ bin/update-data.sh
@@ -4,7 +4,7 @@ printf "Get data\n"
 Rscript data/get-data.R
 
 printf "Updating the target forecast date to today\n"
-Rscript data/forecast-date.R
+## Rscript data/forecast-date.R
 
 printf "Plotting available data\n"
 Rscript data/plot-data.R
